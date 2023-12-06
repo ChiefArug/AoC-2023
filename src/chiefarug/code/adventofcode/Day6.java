23a6ca22c9e86a72a88c8861bd1cba7f5edf9207
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2023-12-06 23:46:44 +1300

    Day 6: Part 2

--- src/chiefarug/code/adventofcode/Day6.java
+++ src/chiefarug/code/adventofcode/Day6.java
@@ -4,18 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Day6 implements Day {
 
     record QuadraticSolution(double min, double max) {
         @SuppressWarnings({"SpellCheckingInspection", "SameParameterValue"})
-        static QuadraticSolution solve(int a, int b, int c) {
+        static QuadraticSolution solve(long a, long b, long c) {
             /*
             x = b +- sqrt(b^2 - 4ac) / 2a
              */
-            int bSquared = b * b;
-            int fourac = 4 * a * c;
-            int twoa = 2 * a;
+            long bSquared = b * b;
+            long fourac = 4 * a * c;
+            long twoa = 2 * a;
             double sqrted = Math.sqrt(bSquared - fourac);
             double topHalfPositive = -b + sqrted;
             double topHalfNegative = -b - sqrted;
@@ -26,28 +27,23 @@ public class Day6 implements Day {
         }
     }
 
-    record Race(int time, int recordDistance) {}
+    record Race(long time, long recordDistance) {
+    }
 
     @Override
     public void run(BufferedReader input) {
-        long count = 1;
+        long count;
         try {
-            String[] firstLine = Arrays.stream(input.readLine().substring(9).split(" ")).filter(Predicate.not(String::isEmpty)).toArray(String[]::new);
-            String[] secondLine = Arrays.stream(input.readLine().substring(9).split(" ")).filter(Predicate.not(String::isEmpty)).toArray(String[]::new);
-            Race[] races = new Race[firstLine.length];
-
-            for (int i = 0; i < firstLine.length; i++) {
-                races[i] = new Race(Integer.parseInt(firstLine[i].strip()), Integer.parseInt(secondLine[i].strip()));
-            }
+            String firstLine = Arrays.stream(input.readLine().substring(9).split(" ")).filter(Predicate.not(String::isEmpty)).collect(Collectors.joining());
+            String secondLine = Arrays.stream(input.readLine().substring(9).split(" ")).filter(Predicate.not(String::isEmpty)).collect(Collectors.joining());
+            Race race = new Race(Long.parseLong(firstLine), Long.parseLong(secondLine));
 
 
-            for (Race race : races) {
-                QuadraticSolution timesForRecord = QuadraticSolution.solve(-1, race.time, -race.recordDistance);
-                int minTime = ((int) Math.floor(timesForRecord.min)) + 1;
-                int maxTime = ((int) Math.ceil(timesForRecord.max)) - 1;
-                int possibilities = maxTime - minTime + 1;
-                count *= possibilities;
-            }
+            QuadraticSolution timesForRecord = QuadraticSolution.solve(-1, race.time, -race.recordDistance);
+            long minTime = ((long) Math.floor(timesForRecord.min)) + 1;
+            long maxTime = ((long) Math.ceil(timesForRecord.max)) - 1;
+            count = maxTime - minTime + 1;
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
