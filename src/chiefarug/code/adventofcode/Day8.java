8db6c702c5e446a7c07295bfc4437bd584367c87
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2023-12-08 19:22:32 +1300

    Day 8: Part 2

--- src/chiefarug/code/adventofcode/Day8.java
+++ src/chiefarug/code/adventofcode/Day8.java
@@ -2,6 +2,7 @@ package chiefarug.code.adventofcode;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -26,21 +27,34 @@ public class Day8 implements Day {
                     .map(s -> new Node(s.substring(0, 3), new PathChoice(s.substring(7, 10), s.substring(12, 15))))
                     .collect(Collectors.toMap(Node::name, Node::next));
 
-            int steps = 0;
-            String currentNode = "AAA";
-            do {
-                boolean shouldGoLeft = leftArray[steps % leftArray.length];
-                PathChoice pathChoice = map.get(currentNode);
-                currentNode = shouldGoLeft ? pathChoice.left() : pathChoice.right();
-                steps++;
-            } while (!currentNode.equals("ZZZ"));
+            String[] startNodes = map.keySet().stream().filter(s -> s.endsWith("A")).toArray(String[]::new);
+            BigInteger lcm = BigInteger.valueOf(getSteps(leftArray, map, startNodes[0]));
+            for (int i = 1; i < startNodes.length; i++) {
+                String startNode = startNodes[i];
+                lcm = lcm(lcm, BigInteger.valueOf(getSteps(leftArray, map, startNode)));
+            }
 
-            System.out.println(steps);
+            System.out.println(lcm);
 
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private BigInteger lcm(BigInteger a, BigInteger b) {
+        return a.multiply(b).abs().divide(a.gcd(b));
+    }
+
+    private static int getSteps(boolean[] leftArray, Map<String, PathChoice> map, String startNode) {
+        int steps = 0;
+        do {
+            boolean shouldGoLeft = leftArray[steps % leftArray.length];
+            PathChoice pathChoice = map.get(startNode);
+            startNode = shouldGoLeft ? pathChoice.left() : pathChoice.right();
+            steps++;
+        } while (!startNode.endsWith("Z"));
+        return steps;
     }
 
     @Override
