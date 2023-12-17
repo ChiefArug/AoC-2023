dcbf77433c8eddb009321f10e20c20f91d2fed40
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2023-12-17 15:50:18 +1300

    Day 16: Part 2 (with FREE visualisation window)

--- src/chiefarug/code/adventofcode/Day16.java
+++ src/chiefarug/code/adventofcode/Day16.java
@@ -1,12 +1,23 @@
 package chiefarug.code.adventofcode;
 
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.EnumSet;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
+import java.util.WeakHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
@@ -18,11 +29,13 @@ public class Day16 implements Day {
         private final List<Direction> selfAsList = List.of(this);
         private final Supplier<List<Direction>> neighbours = new Supplier<>() {
             private List<Direction> value;
+
             @Override
             public List<Direction> get() {
                 return value == null ? value = List.of(Direction.this.clockwise(), Direction.this.counterClockwise()) : value;
             }
         };
+
         List<Direction> asList() {
             return selfAsList;
         }
@@ -116,7 +129,9 @@ public class Day16 implements Day {
             gridRep = gridRepresentation;
         }
 
-        /** @return It was super effective! */
+        /**
+         * @return It was super effective!
+         */
         abstract List<Direction> lightUsesSlam(Direction hitDir);
 
         @Override
@@ -125,29 +140,32 @@ public class Day16 implements Day {
         }
     }
 
-    record Pos(int y, int x) {}
+    record Pos(int y, int x) {
+    }
 
     static class Beam {
+        private static final Object ACTIVE = new Object();
         private final Contraption contraption;
         private Pos currentPos;
         private final Direction movementDir;
-        public Beam(Contraption contraption) {
-            this(contraption, new Pos(0, -1), Direction.EAST);
-        }
 
-        private Beam(Contraption contraption, Pos startPos, Direction startDirection) {
+        Beam(Contraption contraption, Pos startPos, Direction startDirection) {
             this.contraption = contraption;
             this.currentPos = startPos;
             this.movementDir = startDirection;
         }
 
         Stream<Beam> move() {
-            if (isInMap()) contraption.shineUpon(currentPos);
+            if (isInMap() && contraption.whatsHitHere[currentPos.y][currentPos.x] == null)
+                contraption.whatsHitHere[currentPos.y][currentPos.x] = EnumSet.of(movementDir);//contraption.shineUpon(currentPos);
+
             currentPos = movementDir.move(currentPos);
             if (!isInMap()) return Stream.of();
             return contraption.get(currentPos).lightUsesSlam(movementDir).stream()
                     .map(this::fromHitDirection)
-                    .filter(Beam::isInMap);
+                    .filter(Beam::isInMap)
+                    .filter(Beam::isSpotVisited)
+                    .peek(b -> contraption.activeBeams.put(b, ACTIVE));
         }
 
         private boolean isInMap() {
@@ -158,22 +176,27 @@ public class Day16 implements Day {
             return new Beam(contraption, currentPos, dir);
         }
 
+        private boolean isSpotVisited() {
+            Set<Direction> whatsHitHereBefore = contraption.whatsHitHere[currentPos.y][currentPos.x];
+            if (whatsHitHereBefore == null) {
+                contraption.whatsHitHere[currentPos.y][currentPos.x] = EnumSet.of(movementDir);
+                return true;
+            }
+            return whatsHitHereBefore.add(movementDir);
+        }
     }
 
-    record Contraption(Tile[][] map, boolean[][] lightMap) {
+    record Contraption(Tile[][] map, Set<Direction>[][] whatsHitHere,
+                       WeakHashMap<Beam, Object> activeBeams) implements Comparable<Contraption> {
         Tile get(Pos pos) {
             return map[pos.y][pos.x];
         }
 
-        void shineUpon(Pos pos) {
-            lightMap[pos.y][pos.x] = true;
-        }
-
         int countShinies() {
             int counter = 0;
-            for (boolean[] booleans : lightMap) {
-                for (boolean aBoolean : booleans) {
-                    if (aBoolean) counter++;
+            for (Set<Direction>[] booleans : whatsHitHere) {
+                for (Set<Direction> setHere : booleans) {
+                    if (setHere != null && !setHere.isEmpty()) counter++;
                 }
             }
             return counter;
@@ -184,26 +207,17 @@ public class Day16 implements Day {
             StringBuilder stringBuilder = new StringBuilder(map.length * (map[0].length + 1));
             for (int y = 0; y < map.length; y++) {
                 for (int x = 0; x < map[y].length; x++) {
-                    if (lightMap[y][x]) stringBuilder.append('#');
+                    if (whatsHitHere[y][x] != null && !whatsHitHere[y][x].isEmpty()) stringBuilder.append('#');
                     else stringBuilder.append(map[y][x].gridRep);
                 }
                 stringBuilder.append('\n');
             }
             return stringBuilder.toString();
         }
-    }
-    record LightMap(boolean[][] map) {
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj instanceof LightMap lm)
-                return Arrays.deepEquals(map, lm.map);
-            return false;
-        }
 
         @Override
-        public int hashCode() {
-            return Arrays.deepHashCode(map);
+        public int compareTo(@NotNull Day16.Contraption o) {
+            return Integer.compare(countShinies(), o.countShinies());
         }
     }
 
@@ -215,39 +229,72 @@ public class Day16 implements Day {
                 .map(is -> is.mapToObj(i -> Tile.byGridChar.get((char) i)).toArray(Tile[]::new))
                 .toArray(Tile[][]::new);
 
-        Contraption contraption = new Contraption(map, new boolean[map.length][map[0].length]);
+        int permutations = map.length * 2 + map[0].length * 2;
 
-        Beam beam = new Beam(contraption);
+        Contraption[] contraptions = new Contraption[permutations];
+        Arrays.setAll(contraptions, _i -> new Contraption(map, new Set[map.length][map[0].length], new WeakHashMap<>()));
 
-        List<Beam> beams = List.of(beam);
-        Set<LightMap> previousLights = new HashSet<>();
-        int sameCounter = 0;
+
+        List<Beam> beams = new ArrayList<>(permutations);
+        int i = 0;
+        for (int y = 0; y < map.length; y++) {
+            beams.add(new Beam(contraptions[i++], new Pos(y, -1), Direction.EAST));
+            beams.add(new Beam(contraptions[i++], new Pos(y, map[0].length), Direction.WEST));
+        }
+        for (int x = 0; x < map[0].length; x++) {
+            beams.add(new Beam(contraptions[i++], new Pos(-1, x), Direction.SOUTH));
+            beams.add(new Beam(contraptions[i++], new Pos(map.length, x), Direction.NORTH));
+        }
+
+
+        AtomicInteger watched = new AtomicInteger(new Random().nextInt(contraptions.length));
+
+        JFrame frame = new JFrame("Contraption " + watched.get());
+        JTextArea textArea = new JTextArea("hi", 110, 110);
+        textArea.setBackground(Color.BLACK);
+        textArea.setForeground(Color.YELLOW);
+        textArea.setFont(new Font("Monocraft", Font.PLAIN, 4));
+        frame.add(textArea);
+        frame.pack();
+        frame.setVisible(true);
+        textArea.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    frame.setTitle("Component " + watched.addAndGet(1));
+                } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    frame.setTitle("Component " + watched.addAndGet(-1));
+                }
+                textArea.setText(contraptions[watched.get()].toString());
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+
         while (!beams.isEmpty()) {
             // i wish streams could be non lazy so this re-allocation mess didn't have to happen. oh well, the gc will deal with it
             beams = beams.stream().flatMap(Beam::move).toList();
-            System.out.println(contraption);
 
-            if (!previousLights.add(new LightMap(deepCopy(contraption.lightMap))) && ++sameCounter > 100) { //TODO: better loop detection. if we hit a mirror or split that is already lit we can stop that beam from calculating further.
-                break; // were probably in a loop
-            }
+            textArea.setText(contraptions[watched.get()].toString());
 
             try {
-                Thread.sleep(50); // looks pretty in the console
+                Thread.sleep(50);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
 
 
-        System.out.println(contraption.countShinies());
-    }
-
-    private boolean[][] deepCopy(boolean[][] in) {
-        boolean[][] out = new boolean[in.length][];
-        for (int i = 0; i < in.length; i++) {
-            out[i] = Arrays.copyOf(in[i], in[i].length);
-        }
-        return out;
+        Arrays.stream(contraptions).max(Comparator.naturalOrder()).ifPresent(s -> {
+            System.out.println(s);
+            System.out.println(s.countShinies());
+        });
     }
 
     @Override
